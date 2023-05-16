d41a3e754b09c49260a676d4319fdcaac2688a0f
laurasfelix <laurasfelix@hotmail.com.br>
2023-05-15 19:27:54 -0500

    true

--- ELCON-Charger.h
+++ ELCON-Charger.h
@@ -79,7 +79,7 @@ private:
     MakeUnsignedCANSignal(uint16_t, 0, 16, 0.1, 0) Max_Allowable_Charging_Terminal_Voltage{};
     MakeUnsignedCANSignal(uint16_t, 16, 16, 0.1, 0) Max_Allowable_Charging_Current{};
     MakeUnsignedCANSignal(bool, 32, 1, 1, 0) Control{};
-    CANTXMessage<3> Message1{can_interface_, 0x1806E5F4, 8, 500, Max_Allowable_Charging_Terminal_Voltage,
+    CANTXMessage<3> Message1{can_interface_, 0x1806E5F4, true, 8, 500, Max_Allowable_Charging_Terminal_Voltage,
                              Max_Allowable_Charging_Current, Control};
 
     MakeUnsignedCANSignal(uint16_t, 0, 16, 0.1, 0) Output_Voltage{};
